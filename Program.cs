d42f11434b61a3dcebaa3af300fdc70a6a7dde29
s25101 <s25101@pjwstk.edu.pl>
2024-04-11 21:40:43 +0200

    Calculate Average Method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Modyfikacja 3");
+﻿using System;
+using System.Linq;
+
+public static class Statistics
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return numbers.Average();
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        double average = Statistics.CalculateAverage(numbers);
+        Console.WriteLine("Średnia wynosi: " + average);
+    }
+}
