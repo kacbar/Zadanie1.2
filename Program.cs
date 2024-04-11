4e03a6cb9853334a95ab4e4436e4eb169d8b1385
s25101 <s25101@pjwstk.edu.pl>
2024-04-11 21:49:13 +0200

    Calculate Max Value Method

--- Program.cs
+++ Program.cs
@@ -14,6 +14,15 @@ public static class Statistics
             return numbers.Average();
         }
     }
+    
+    public static int CalculateMaxValue(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            return 0;
+        }
+        return numbers.Max();
+    }
 }
 class Program
 {
@@ -22,5 +31,8 @@ class Program
         int[] numbers = { 1, 2, 3, 4, 5 };
         double average = Statistics.CalculateAverage(numbers);
         Console.WriteLine("Średnia wynosi: " + average);
+
+        double max = Statistics.CalculateMaxValue(numbers);
+        Console.WriteLine("Maksymalna wartośc wynosi: " + max);
     }
 }
